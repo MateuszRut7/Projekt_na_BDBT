5670ba4c59a6490cfbb6588bd60914ca84500c2c
Mateusz Marek Rutkowski <m.rutkowski@stud.elka.pw.edu.pl>
2021-01-27 20:11:54 +0100

    front

--- jednostka_akademicka/src/main/java/bdbt/jednostka_akademicka/wynagrodzenie/Wynagrodzenie.java
+++ jednostka_akademicka/src/main/java/bdbt/jednostka_akademicka/wynagrodzenie/Wynagrodzenie.java
@@ -5,12 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Wynagrodzenie {
     private int nrWynagrodzenia;
+
+    @Pattern(regexp="^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$")
     private String dataWynagrodzenia;
+
     private double kwotaPod;
     private double kwotaDod;
     private int nrPracownika;
